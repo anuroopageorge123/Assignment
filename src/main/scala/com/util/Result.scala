cef960f9e0297ff7aade6fe224b1b9ccfe8826d3
Anuroopa George <anuroopageorge2dec@yahoo.co.in>
2018-08-05 22:43:15 -0400

    1st commit

---
+++ src/main/scala/com/util/Result.scala
@@ -0,0 +1,11 @@
+package com.util
+
+/*
+palindromes in a string, and print them, along with their starting position and length
+ */
+case class Result
+(
+  subString:String,
+  index:Int,
+  size :Long
+)
